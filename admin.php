373aa1952e1c057890148d110e57cb72bd73aaae
bunty96195 <44460012+bunty96195@users.noreply.github.com>
2018-10-25 13:01:45 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/symbol.png" />
+        <title>Shop4U Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+          <br><br><br>
+           <div class="container">
+                <div class="row">
+                    <div class="col-xs-6 col-xs-offset-3">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h3> ADMIN LOGIN</h3>
+                            </div>
+                            <div class="panel-body">
+                                <form method="POST" action="check_admin.php">
+                                    <div class="form-group">
+                                        <input type="name" class="form-control" name="username" placeholder="username">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" name="Password" placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" value="Login" class="btn btn-primary">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+
+    				
+
---
+++ check_admin.php
@@ -0,0 +1,23 @@
+
+<html>
+<title>Login</title>
+<body>
+<?php
+  $usr = "admin";
+  $psw = "password";
+
+  //$usr == $username && $psw == $password
+  session_start();
+  if ( ($_POST['username']=="admin" && $_POST['Password']=="password")) {
+    echo "password accepted";
+    $_SESSION['login']=true;}
+    else {
+      echo "incorrect login";
+    }
+    ?>
+
+  <form name="input" action="adminportal.php" method="get">
+    <input type="submit" value="Home">
+  </form>
+</body>
+</html>
